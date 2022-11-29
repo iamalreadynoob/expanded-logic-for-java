89099ac46a5f1c4cb4898a3059097494157b29c0
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-29 07:11:25 +0300

    Update Xor.java

--- derivedMethods/Xor.java
+++ derivedMethods/Xor.java
@@ -1,5 +1,6 @@
 package derivedMethods;
 
+import situation.ExpandedLogicCase;
 import situation.LogicCase;
 
 public class Xor {
@@ -9,6 +10,29 @@ public class Xor {
 		if((situation.getFirst() == true && situation.getSecond() == true) ||
 			(situation.getFirst() == false && situation.getSecond() == false)) return false;
 		else return true;
+	}
+	
+	public boolean xor(ExpandedLogicCase situation)
+	{
+		if((situation.getFirst() > situation.getReferencePoint() && situation.getSecond() > situation.getReferencePoint()) || 
+		  (situation.getFirst() < situation.getReferencePoint() && situation.getSecond() < situation.getReferencePoint()))
+		{
+			return false;
+		}
+		else return true;
+		
+	}
+	
+	public float specificXor(ExpandedLogicCase situation)
+	{
+		float first = (situation.getFirst() + situation.getSecond()) / 2;
+		float difference = situation.getReferencePoint() - first;
+		first += difference;
+		
+		double multiply = (double)(situation.getFirst() * situation.getSecond());
+		float second = (float)Math.pow(multiply, 0.5);
+		
+		return (first + second) / 2;
 	}
 
 }
