e8ec0fdd0b05697df5242854c9319b932b4cb14f
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-29 07:07:17 +0300

    Add files via upload

---
+++ exceptions/InvalidTruthValueException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InvalidTruthValueException extends Exception
+{
+
+	public InvalidTruthValueException()
+	{
+		super("The value which is assigned is invalid");
+	}
+	
+}
