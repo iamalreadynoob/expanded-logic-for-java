9c1e30544f66de336831287b20b17d01f1fb601d
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-29 07:09:03 +0300

    Update Or.java

--- fundamentalMethods/Or.java
+++ fundamentalMethods/Or.java
@@ -1,5 +1,6 @@
 package fundamentalMethods;
 
+import situation.ExpandedLogicCase;
 import situation.LogicCase;
 
 public class Or {
@@ -9,6 +10,20 @@ public class Or {
 		
 		if(situation.getFirst() || situation.getSecond()) return true;
 		else return false;
+		
+	}
+	
+	public boolean or(ExpandedLogicCase situation)
+	{
+		if(situation.getFirst() < situation.getReferencePoint() && situation.getSecond() < situation.getReferencePoint()) return false;
+		else return true;
+	}
+	
+	public float specificOr(ExpandedLogicCase situation)
+	{
+		double multiply = (double)(situation.getFirst() * situation.getSecond());
+		return (float)Math.pow(multiply, 0.5);
+		
 		
 	}
 
