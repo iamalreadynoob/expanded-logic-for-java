737b75847dc17d1409b4f854f1f9f9f5d81a7bfe
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-29 07:12:22 +0300

    Update IfAndOnlyIf.java

--- derivedMethods/IfAndOnlyIf.java
+++ derivedMethods/IfAndOnlyIf.java
@@ -1,5 +1,6 @@
 package derivedMethods;
 
+import situation.ExpandedLogicCase;
 import situation.LogicCase;
 
 public class IfAndOnlyIf {
@@ -11,6 +12,35 @@ public class IfAndOnlyIf {
 		   (situation.getFirst() == false && situation.getSecond() == false)) return true;
 		else return false;
 		
+	}
+	
+	public boolean ifAndOnlyIf(ExpandedLogicCase situation)
+	{
+		if((situation.getFirst() > situation.getReferencePoint() && situation.getSecond() > situation.getReferencePoint()) || 
+		   (situation.getFirst() < situation.getReferencePoint() && situation.getSecond() < situation.getReferencePoint()))
+		{
+			return true;
+		}
+		else return false;
+	}
+	
+	public float specificIfAndOnlyIf(ExpandedLogicCase situation)
+	{
+		float difference = situation.getReferencePoint() - situation.getFirst();
+		
+		float notFirst = situation.getReferencePoint() + difference;
+		
+		difference = situation.getReferencePoint() - situation.getSecond();
+		float notSecond = situation.getReferencePoint() + difference;
+		
+		double multiply = (double)(notFirst * situation.getSecond());
+		
+		float valueFirst = (float)Math.pow(multiply, 0.5);
+		
+		multiply = (double)(situation.getFirst() * notSecond);
+		float valueSecond = (float)Math.pow(multiply, 0.5);
+		
+		return (valueFirst + valueSecond) / 2;
 	}
 	
 }
