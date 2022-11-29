a61d2b6d1560e8ce58bc3cf8192fed07ad458f65
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-11-29 07:11:57 +0300

    Update IfThen.java

--- derivedMethods/IfThen.java
+++ derivedMethods/IfThen.java
@@ -1,5 +1,6 @@
 package derivedMethods;
 
+import situation.ExpandedLogicCase;
 import situation.LogicCase;
 
 public class IfThen {
@@ -9,6 +10,24 @@ public class IfThen {
 		
 		if(situation.getFirst() == true && situation.getSecond() == false) return false;
 		else return true;
+		
+	}
+	
+	public boolean ifThen(ExpandedLogicCase situation)
+	{
+		
+		if(situation.getFirst() > situation.getReferencePoint() && situation.getSecond() < situation.getReferencePoint()) return false;
+		else return true;
+		
+	}
+	
+	public float specificIfThen(ExpandedLogicCase situation)
+	{
+		float difference = situation.getReferencePoint() - situation.getFirst();
+		float first = situation.getReferencePoint() + difference;
+		
+		double multiply = (double)(first * situation.getSecond());
+		return (float)Math.pow(multiply, 0.5);
 		
 	}
 	
